965623d1b44335e2fda2fd83ddba54cf8918354c
Gleb Pitsevich <pitsevich@gmail.com>
2013-12-16 16:18:56 +0300

    Adding models draft - SmartCache and SmartCacheParam. Adds cache-like interface to get, set methods.

---
+++ smartcache/models.py
@@ -0,0 +1,82 @@
+from django.db import models
+
+
+class SmartCacheQuerySet(models.query.QuerySet):
+    def filter(self, *args, **kwargs):
+        kwargs.pop('valid', '')
+        only_valid = kwargs.pop('only_valid', True)
+        mod_kwargs = {}
+        for k,v in kwargs.items():
+            mod_kwargs['param_set__name'] = k
+            mod_kwargs['param_set__value'] = v
+        if only_valid:
+            mod_kwargs['valid'] = True
+
+        return super(SmartCacheQuerySet, self).\
+            filter(*args, **mod_kwargs).distinct()
+
+    def get(self, *args, **kwargs):
+        return super(SmartCacheQuerySet, self).get(*args, **kwargs)
+
+
+class SmartCacheManager(models.Manager):
+    def get_query_set(self):
+        return SmartCacheQuerySet(self.model, using=self._db)
+
+    def invalidate(self, *args, **kwargs):
+        kwargs['only_valid'] = False
+        self.filter(*args, **kwargs).update(valid=False)
+
+    def create(self, value, *args, **kwargs):
+        smart_cache = self.model(value=value)
+        smart_cache.save()
+        for k, v in kwargs.items():
+            cache_param = SmartCacheParam(name=k, value=v, cache=smart_cache)
+            cache_param.save()
+
+    def get(self, *args, **kwargs):
+        return self.get_query_set().get(*args, **kwargs).value
+
+    def get_many(self, *args, **kwargs):
+        return self.filter(*args, **kwargs).values_list('value', flat=True)
+
+
+class SmartCache(models.Model):
+    value = models.TextField()
+    valid = models.BooleanField(default=True)
+
+    objects = SmartCacheManager()
+    _objects = models.Manager()
+
+    @classmethod
+    def set(cls, value, *args, **kwargs):
+        cls.objects.create(value, *args, **kwargs)
+
+    @classmethod
+    def get(cls, *args, **kwargs):
+        values = cls.objects.get(*args, **kwargs)
+        return values
+
+    @classmethod
+    def get_many(cls, *args, **kwargs):
+        return cls.objects.get_many(*args, **kwargs)
+
+    @classmethod
+    def delete(cls, *args, **kwargs):
+        return cls.objects.invalidate(*args, **kwargs)
+
+    def __unicode__(self):
+        return 'Cache '+ ', '.join(
+            ['%s=%s' % (p.name, p.value) for p in self.param_set.all()])
+
+
+class SmartCacheParam(models.Model):
+    name = models.CharField(max_length=500)
+    value = models.CharField(max_length=500)
+    cache = models.ForeignKey(SmartCache, related_name='param_set')
+
+    class Meta:
+        unique_together = ('name', 'cache')
+
+    def __unicode__(self):
+        return 'Key Param %s=%s' % (self.name, self.value)
