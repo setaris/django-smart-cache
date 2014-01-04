e7f52a7d6c067b275dcb7009f0c74393caf2dc6b
Gleb Pitsevich <pitsevich@gmail.com>
2014-01-04 17:25:19 +0300

    Adding setup.py file.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+install_requires = [
+    'django >= 1.4',
+]
+
+setup(
+    name = "django-smart-cache",
+    version = '0.1',
+    description = "A Django library that allows flexible database caching" +
+        " using composite caching keys.",
+    url = "https://github.com/setaris/django-smart-cache",
+    author = "Setaris",
+    author_email = "support@setaris.com",
+    packages = find_packages(),
+    install_requires = install_requires,
+)
